f1026fd8c2249a395a5e418253f7e517ad025e34
Ricardo Escalante <escalante.ricardo06@gmail.com>
2025-01-31 01:13:34 -0800

    Basic algorithm to check for new songs

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from ytmusicapi import YTMusic
-from ytmusicapi.auth.oauth import OAuthCredentials  # Make sure to import OAuthCredentials
+from ytmusicapi.auth.oauth import OAuthCredentials
+from dotenv import load_dotenv
+import os
+import time
 
-ytmusic = YTMusic('oauth.json', oauth_credentials=OAuthCredentials(client_id="your_client_id", client_secret="your_client_secret"))
+load_dotenv()
 
-print(ytmusic.get_history()[0]["title"])
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+ytmusic = YTMusic('oauth.json', oauth_credentials=OAuthCredentials(client_id=client_id, client_secret=client_secret))
+
+On = True
+
+last_name = ytmusic.get_history()[0]["title"]
+last_id = ytmusic.get_history()[0]["videoId"]
+
+print(last_name)
+
+while On:
+    current_id = ytmusic.get_history()[0]["videoId"]
+
+    if current_id != last_id:
+        print(ytmusic.get_song(current_id)["videoDetails"]["title"])
+        last_id = current_id
+        print("Song Changed!")
+    else:
+        print("No Change")
+    
+    time.sleep(2)
